e3b6cc5ce724c638799317efc47bde66ea84c64d
nirob <nhnirob362@gmail.com>
2025-08-27 11:44:42 +0600

    calling function done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 // .......reuseable code............../
+
 //string to number convert
 function strToNum(id) {
   const numVar = parseInt(document.getElementById(id).innerText);
@@ -31,21 +32,62 @@ for (const hart of allHart) {
 }
 
 // call function
+const callData = [];
+
 const callFunc = document.getElementsByClassName("call");
 for (const calls of callFunc) {
   calls.addEventListener("click", function () {
-    const parentDiv=calls.closest('div')
-    const serviceName=parentDiv.querySelector('.services-name').innerText;
-    const serviceNum=parentDiv.querySelector('.services-num').innerText;
+    const parentDiv = calls.closest("div");
+    const serviceName = parentDiv.querySelector(".services-name").innerText;
+    const callServicesName = parentDiv.querySelector(".callServices").innerText;
+    const serviceNum = parentDiv.querySelector(".services-num").innerText;
     const coinValue = strToNum("coins");
     if (coinValue < 20) {
-      alert( 
+      alert(
         "  You don't have enough coins.You need at least 20 coins to make a call"
       );
       return;
     }
     alert(`calling ${serviceName} ${serviceNum}...`);
-    const tottalCoin=coinValue-20;
-    document.getElementById('coins').innerText=tottalCoin
+    const tottalCoin = coinValue - 20;
+    document.getElementById("coins").innerText = tottalCoin;
+
+    const data = {
+      name: callServicesName,
+      num: serviceNum,
+      date: new Date().toLocaleTimeString(),
+    };
+    callData.push(data);
+    const parent = document.getElementById("history-container");
+    parent.innerText = "";
+
+    for (const value of callData) {
+      const div = document.createElement("div");
+      div.innerHTML = `
+      
+          <div id='div' class="flex justify-between items-center px-3 mb-3">
+            
+            <div class="">
+              <h2 class="font-bold text-[14px]">${value.name}</h2>
+              <p class="font-semibold text-xs text-gray-500">${value.num}</p>
+            </div>
+            <p class="text-xs">${value.date}</p>
+          </div>
+
+        
+      `;
+      parent.appendChild(div);
+    }
   });
 }
+
+// clear history
+document.getElementById('clear').addEventListener("click",function(){
+  const parent = document.getElementById("history-container");
+  parent.innerText = ''; 
+  callData.length=0;
+
+
+
+})
+
